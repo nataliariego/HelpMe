54a7f357d6f2c53f00862ecd84e4e93059d969c3
FranciscoCoya <kikocoya@gmail.com>
2022-10-27 21:30:41 +0200

    Integración Firebase con proyecto Android.

--- app/src/main/java/com/example/helpme/HomeActivity.java
+++ app/src/main/java/com/example/helpme/HomeActivity.java
@@ -1,12 +1,21 @@
 package com.example.helpme;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.helpme.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
@@ -19,6 +28,8 @@ public class HomeActivity extends AppCompatActivity {
     private TextView txDayOfTheWeek;
     private TextView txDateFormatted;
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         initCalendarData();
+
+        readData();
     }
 
     /**
@@ -42,11 +55,25 @@ public class HomeActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             txDayOfTheWeek.setText(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("es")).toUpperCase());
             txDateFormatted.setText(LocalDate.now().getDayOfMonth() + " "
-                    + LocalDate.now().getMonth().getDisplayName(TextStyle.FULL, new Locale("es")).toUpperCase()
+                    + LocalDate.now().getMonth().getDisplayName(TextStyle.SHORT, new Locale("es")).replace(".", "").toUpperCase()
                     + " " + LocalDate.now().getYear());
         }
+    }
 
-
-
+    private void readData() {
+        db.collection("DUDA")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
