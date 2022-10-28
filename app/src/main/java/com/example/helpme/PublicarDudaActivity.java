68aa9801323f62b4a1c8dfa4e7702bea1cc82253
Jose Rodriguez Rico <joserrti08@gmail.com>
2022-10-28 15:55:03 +0200

    Activity publicar duda sin base de datos

---
+++ app/src/main/java/com/example/helpme/PublicarDudaActivity.java
@@ -0,0 +1,17 @@
+package com.example.helpme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PublicarDudaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publicar_duda);
+
+
+
+    }
+}
