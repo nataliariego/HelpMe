9bb93248e688c17b0e2604c9c96bc7516d676e22
FranciscoCoya <kikocoya@gmail.com>
2022-11-04 15:07:05 +0100

    Completed Create an account View UI without functionality

---
+++ app/src/main/java/com/example/helpme/CreateAccountActivity.java
@@ -0,0 +1,18 @@
+package com.example.helpme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        setTitle("Crear una cuenta");
+
+
+    }
+}
