376c404a36b841192ae20c39e43d7040cc8ed9ae
FranciscoCoya <kikocoya@gmail.com>
2022-11-04 14:49:39 +0100

    Completed Login View without functionality, only UI

--- app/src/main/java/com/example/helpme/LoginActivity.java
+++ app/src/main/java/com/example/helpme/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.helpme;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Iniciar sesión");
+
+        btLogin = (Button) findViewById(R.id.button_login);
+
     }
 }
