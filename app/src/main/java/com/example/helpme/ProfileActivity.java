a190a48097cec3c4880aecab8371b0b5ed0401ba
uo277516 <90771958+uo277516@users.noreply.github.com>
2022-11-12 19:12:21 +0100

    Creado editar perfil. Falta: -Multiple choice en el spinner de curso -Coger usuario del auth y actualizarlo -Mirar tema fotos en listaod dudas

--- app/src/main/java/com/example/helpme/ProfileActivity.java
+++ app/src/main/java/com/example/helpme/ProfileActivity.java
@@ -13,8 +13,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dto.AsignaturaDto;
+import dto.CursoDto;
 import dto.DudaDto;
 import viewmodel.AsignaturaViewModel;
+import viewmodel.CursoViewModel;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -24,15 +26,45 @@ public class ProfileActivity extends AppCompatActivity {
     private Spinner spinnerAsignaturas;
     private List<String> nombreAsignaturas = new ArrayList<>();
 
+    private List<CursoDto> cursos = new ArrayList<CursoDto>();
+    private CursoViewModel cursoViewModel = new CursoViewModel();
+    private Spinner spinnerCursos;
+    private List<String> numeroCursos = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         cargarAsignaturas();
+        cargarCursos();
 
 
+    }
 
+    private void cargarCursos() {
+        cursoViewModel.getAllCursos().observe(this, dudasResult -> {
+            if (dudasResult != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    dudasResult.forEach(d -> {
+                        Log.i(TAG, d.getNumero());
+                        CursoDto a = new CursoDto();
+                        a.numero = d.getNumero();
+
+                        cursos.add(a);
+                    });
+                }
+            }
+            for (CursoDto dto: cursos
+            ) {
+                numeroCursos.add(dto.numero);
+            }
+
+
+            spinnerCursos = findViewById(R.id.spinnerCurso);
+            spinnerCursos.setAdapter(new ArrayAdapter<>(ProfileActivity.this, android.R.layout.simple_spinner_dropdown_item, numeroCursos));
+
+        });
     }
 
     private void cargarAsignaturas() {
@@ -54,15 +86,8 @@ public class ProfileActivity extends AppCompatActivity {
                 nombreAsignaturas.add(dto.nombre);
             }
 
-            Log.i("pepito", String.valueOf(nombreAsignaturas.size()));
-
-            for (String s: nombreAsignaturas
-            ) {
-                Log.i("paaaaaaaaa", s);
-            }
-
             spinnerAsignaturas = findViewById(R.id.spinnerAsignaturasProfile);
-            spinnerAsignaturas.setAdapter(new ArrayAdapter<>(ProfileActivity.this, android.R.layout.simple_spinner_dropdown_item, nombreAsignaturas));
+            spinnerAsignaturas.setAdapter(new ArrayAdapter<>(ProfileActivity.this, android.R.layout.simple_selectable_list_item, nombreAsignaturas));
 
         });
     }
