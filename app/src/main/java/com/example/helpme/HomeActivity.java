1f4f23c84cf05e0900ebf1e743c18fa60f29b249
FranciscoCoya <kikocoya@gmail.com>
2023-01-08 15:37:53 +0100

    refactor: remove unused code and comments in home activity

--- app/src/main/java/com/example/helpme/HomeActivity.java
+++ app/src/main/java/com/example/helpme/HomeActivity.java
@@ -1,14 +1,13 @@
 package com.example.helpme;
 
+import android.annotation.SuppressLint;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,20 +34,12 @@ public class HomeActivity extends AppCompatActivity implements NetworkStatusHand
     public static final String TAG = "HOME_ACTIVITY";
     public static final String DUDA_SELECCIONADA = "duda_seleccionada";
 
-    private TextView txDayOfTheWeek;
-    private TextView txDateFormatted;
-    private Button btVerTodasDudas;
     private RecyclerView listadoDudasHomeRecycler;
-    private Button btNuevaDuda;
 
     private DudaAdapter dudaAdapter;
-    private DudaViewModel dudaViewModel = new DudaViewModel();
+    private final DudaViewModel dudaViewModel = new DudaViewModel();
+    private final List<DudaDto> dudas = new ArrayList<>();
 
-    private List<DudaDto> dudas = new ArrayList<>();
-
-    private BottomNavigationView navegacion;
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -62,8 +53,8 @@ public class HomeActivity extends AppCompatActivity implements NetworkStatusHand
             finish();
         }
 
-        btVerTodasDudas = (Button) findViewById(R.id.btVerTodasDudas);
-        btNuevaDuda = (Button) findViewById(R.id.bt_nueva_duda_home); // Es un layout no un boton
+        Button btVerTodasDudas = (Button) findViewById(R.id.btVerTodasDudas);
+        Button btNuevaDuda = (Button) findViewById(R.id.bt_nueva_duda_home); // Es un layout no un boton
         listadoDudasHomeRecycler = (RecyclerView) findViewById(R.id.listado_dudas_home_recycler);
 
         listadoDudasHomeRecycler.setHasFixedSize(true);
@@ -73,24 +64,14 @@ public class HomeActivity extends AppCompatActivity implements NetworkStatusHand
         cargarDudas();
 
         //Navegación
-        navegacion = findViewById(R.id.bottomNavigationView);
+        BottomNavigationView navegacion = findViewById(R.id.bottomNavigationView);
         IntentExtras.getInstance().handleNavigationView(navegacion, getBaseContext());
 
         /* Redirecciones a otras pantallas */
 
-        btVerTodasDudas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                redirectPantallaListadoDudas();
-            }
-        });
+        btVerTodasDudas.setOnClickListener(view -> redirectPantallaListadoDudas());
 
-        btNuevaDuda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                redirectPantallaPublicarNuevaDuda();
-            }
-        });
+        btNuevaDuda.setOnClickListener(view -> redirectPantallaPublicarNuevaDuda());
     }
 
     /**
@@ -111,7 +92,7 @@ public class HomeActivity extends AppCompatActivity implements NetworkStatusHand
     }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("NotifyDataSetChanged")
     private void cargarDudas() {
         dudas.clear();
 
@@ -133,12 +114,7 @@ public class HomeActivity extends AppCompatActivity implements NetworkStatusHand
             }
 
             dudaAdapter = new DudaAdapter(dudas,
-                    new DudaAdapter.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(DudaDto duda) {
-                            clikonIntem(duda);
-                        }
-                    });
+                    this::clikonIntem);
             listadoDudasHomeRecycler.setAdapter(dudaAdapter);
 
             dudaAdapter.notifyDataSetChanged();
@@ -153,16 +129,16 @@ public class HomeActivity extends AppCompatActivity implements NetworkStatusHand
     }
 
     private Duda crearDuda(DudaDto duda) {
-        Duda d = new Duda(duda.titulo, duda.descripcion, duda.alumno, duda.asignatura, duda.materia, duda.isResuelta, duda.fecha, duda.id, duda.url_adnjuto);
-        return d;
+        return new Duda(duda.titulo, duda.descripcion, duda.alumno, duda.asignatura, duda.materia, duda.isResuelta, duda.fecha, duda.id, duda.url_adnjuto);
     }
 
     /**
      * Inicializa el contenido del calendario de la pantalla de home.
      */
+    @SuppressLint("SetTextI18n")
     private void initCalendarData() {
-        txDayOfTheWeek = findViewById(R.id.home_calendar_day_week);
-        txDateFormatted = findViewById(R.id.home_calendar_custom_date_today);
+        TextView txDayOfTheWeek = findViewById(R.id.home_calendar_day_week);
+        TextView txDateFormatted = findViewById(R.id.home_calendar_custom_date_today);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             txDayOfTheWeek.setText(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("es")).toUpperCase());
@@ -174,12 +150,7 @@ public class HomeActivity extends AppCompatActivity implements NetworkStatusHand
 
     @Override
     public void checkConnection() {
-        NetworkStatusChecker.getInstance().handleConnection(getApplicationContext(), new NetworkStatusChecker.ConnectionCallback() {
-            @Override
-            public void callback(boolean isConnected) {
-                handleConnection(isConnected);
-            }
-        });
+        NetworkStatusChecker.getInstance().handleConnection(getApplicationContext(), this::handleConnection);
     }
 
     @Override
