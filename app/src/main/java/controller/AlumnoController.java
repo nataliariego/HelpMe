86e3e2d92f12d375509417ffcb91b941c8519aae
FranciscoCoya <kikocoya@gmail.com>
2023-01-08 15:55:14 +0100

    refactor: remove unused code in alumno controller

--- app/src/main/java/controller/AlumnoController.java
+++ app/src/main/java/controller/AlumnoController.java
@@ -1,16 +1,11 @@
 package controller;
 
-import android.os.Build;
+import android.annotation.SuppressLint;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.helpme.model.Alumno;
-import com.example.helpme.model.Asignatura;
-import com.example.helpme.model.Duda;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -22,15 +17,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
+import java.util.Objects;
 
-import assembler.AlumnoAssembler;
 import dto.AlumnoDto;
 
 public class AlumnoController {
 
     public static final String TAG = "ALUMNO_CONTROLLER";
 
+    @SuppressLint("StaticFieldLeak")
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private static AlumnoController instance;
@@ -46,11 +41,9 @@ public class AlumnoController {
 
     /**
      * Listado de todas las dudas de la aplicación.
-     *
-     * @return
      */
     public MutableLiveData<List<Alumno>> findAll() {
-        MutableLiveData<List<Alumno>> liveAlumnos = new MutableLiveData<List<Alumno>>();
+        MutableLiveData<List<Alumno>> liveAlumnos = new MutableLiveData<>();
 
         db.collection(Alumno.COLLECTION)
                 .addSnapshotListener((snapshot, e) -> {
@@ -67,6 +60,7 @@ public class AlumnoController {
 //                            AlumnoDto aRes = AlumnoAssembler.toDto(documentSnapshot.get(Duda.REF_ALUMNO).toString());
 //                            Log.i(TAG, "ALUMNO CONTROLLER: " + aRes.nombre + " " + aRes.uo);
 
+                            assert alumno != null;
                             alumno.setNombre(documentSnapshot.getString(Alumno.NOMBRE));
                             alumno.setUo(documentSnapshot.getString(Alumno.UO));
                             alumno.setUrl_foto(documentSnapshot.getString(Alumno.URL_FOTO));
@@ -84,7 +78,7 @@ public class AlumnoController {
     }
 
     public MutableLiveData<List<Alumno>> findAllFriendsActivity() {
-        MutableLiveData<List<Alumno>> liveAlumnos = new MutableLiveData<List<Alumno>>();
+        MutableLiveData<List<Alumno>> liveAlumnos = new MutableLiveData<>();
 
         db.collection(Alumno.COLLECTION)
                 .addSnapshotListener((snapshot, e) -> {
@@ -101,10 +95,11 @@ public class AlumnoController {
 //                            AlumnoDto aRes = AlumnoAssembler.toDto(documentSnapshot.get(Duda.REF_ALUMNO).toString());
 //                            Log.i(TAG, "ALUMNO CONTROLLER: " + aRes.nombre + " " + aRes.uo);
 
+                            assert alumno != null;
                             alumno.setNombre(documentSnapshot.getString(Alumno.NOMBRE));
                             alumno.setUo(documentSnapshot.getString(Alumno.UO));
                             alumno.setUrl_foto(documentSnapshot.getString(Alumno.URL_FOTO));
-                            alumno.setAsignaturasDominadas((Map<String,Object>)documentSnapshot.get("asignaturasDominadas"));
+                            alumno.setAsignaturasDominadas((Map<String, Object>) documentSnapshot.get("asignaturasDominadas"));
 
                             alumnos.add(alumno);
                         }
@@ -120,20 +115,62 @@ public class AlumnoController {
 
     /**
      * Obtiene un alumno por su referencia.
-     *
-     * @param ref
      */
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void findById(DocumentReference ref, AlumnoCallback callback) {
         Task<DocumentSnapshot> document = db.document(ref.getPath()).get();
 
-        document.addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot doc = document.getResult();
+        document.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot doc = document.getResult();
+
+                Alumno alumno = getPayload(doc.getId(), doc.getString(Alumno.UO), doc.getString(Alumno.NOMBRE));
+
+                callback.callback(alumno);
+            }
+        });
+    }
+
+    public void findByUO(String uo, AlumnoCallback callback) {
+        Task<QuerySnapshot> document = db.collection(Alumno.COLLECTION).whereEqualTo("uo", uo).get();
+
+
+        document.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                List<DocumentSnapshot> docs = task.getResult().getDocuments();
+
+                if (docs.size() > 0) {
+                    DocumentSnapshot doc = docs.get(0);
 
                     Alumno alumno = getPayload(doc.getId(), doc.getString(Alumno.UO), doc.getString(Alumno.NOMBRE));
+                    callback.callback(alumno);
+                }
+            }
+        });
+    }
+
+
+    public void findByUOWithPhoto(String email, AlumnoCallback callback) {
+        Task<QuerySnapshot> document = db.collection(Alumno.COLLECTION).whereEqualTo("email", email).get();
+
+        document.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                List<DocumentSnapshot> docs = task.getResult().getDocuments();
+
+                Log.i(TAG, "DOCS: " + docs);
+
+                if (docs.size() > 0) {
+                    DocumentSnapshot doc = docs.get(0);
+
+                    Alumno alumno = getPayloadWithUrl(doc.getId(), doc.getString(Alumno.UO), doc.getString(Alumno.NOMBRE), doc.getString(Alumno.URL_FOTO));
+                    //alumno.setUo(doc.getString(Alumno.UO));
+                    alumno.setEmail(doc.getString(Alumno.EMAIL));
+                    alumno.setAsignaturasDominadas((Map<String, Object>) doc.get(Alumno.ASIGNATURAS_DOMINADAS));
+                    alumno.setId(doc.getId());
+                    alumno.setUrl_foto(doc.getString(Alumno.URL_FOTO));
+
+                    Log.i(TAG, "USUARIO EN SESION: " + Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail());
+                    Log.i(TAG, "USER SES METADATA: " + FirebaseAuth.getInstance().getCurrentUser().getMetadata());
+                    Log.i(TAG, "FOTO: " + doc.getString(Alumno.URL_FOTO));
 
                     callback.callback(alumno);
                 }
@@ -141,90 +178,21 @@ public class AlumnoController {
         });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public void findByUO(String uo, AlumnoCallback callback) {
-        Task<QuerySnapshot> document = db.collection(Alumno.COLLECTION).whereEqualTo("uo", uo).get();
-
-
-
-        document.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    List<DocumentSnapshot> docs = task.getResult().getDocuments();
-
-                    if(docs.size() > 0){
-                        DocumentSnapshot doc = docs.get(0);
-
-                        Alumno alumno = getPayload(doc.getId(), doc.getString(Alumno.UO), doc.getString(Alumno.NOMBRE));
-                        callback.callback(alumno);
-                    }
-                }
-            }
-        });
-    }
-
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public void findByUOWithPhoto(String email, AlumnoCallback callback) {
-        Task<QuerySnapshot> document = db.collection(Alumno.COLLECTION).whereEqualTo("email", email).get();
-
-
-
-        document.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    List<DocumentSnapshot> docs = task.getResult().getDocuments();
-
-                    Log.i(TAG, "DOCS: " + docs.toString());
-
-                    if(docs.size() > 0){
-                        DocumentSnapshot doc = docs.get(0);
-
-                        Alumno alumno = getPayloadWithUrl(doc.getId(), doc.getString(Alumno.UO), doc.getString(Alumno.NOMBRE), doc.getString(Alumno.URL_FOTO));
-                        //alumno.setUo(doc.getString(Alumno.UO));
-                        alumno.setEmail(doc.getString(Alumno.EMAIL));
-                        alumno.setAsignaturasDominadas((Map<String,Object>)doc.get(Alumno.ASIGNATURAS_DOMINADAS));
-                        alumno.setId(doc.getId());
-                        alumno.setUrl_foto(doc.getString(Alumno.URL_FOTO));
-
-                        Log.i(TAG, "USUARIO EN SESION: " + FirebaseAuth.getInstance().getCurrentUser().getEmail());
-                        Log.i(TAG, "USER SES METADATA: " + FirebaseAuth.getInstance().getCurrentUser().getMetadata());
-                        Log.i(TAG, "FOTO: " + doc.getString(Alumno.URL_FOTO));
-
-                        callback.callback(alumno);
-                    }
-                }
-            }
-        });
-    }
-
-    public void update(AlumnoDto alumno, String uid){
-
-        //Map<String, Object> alHash = AlumnoAssembler.toHashMap(alumno);
-
+    public void update(AlumnoDto alumno, String uid) {
         alumno.password = "";
 
-        db.collection(Alumno.COLLECTION).document(uid).set(alumno).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Log.d(TAG, "Alumno actualizado");
-                }
+        db.collection(Alumno.COLLECTION).document(uid).set(alumno).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Log.d(TAG, "Alumno actualizado");
             }
         });
     }
 
-    public void delete(){
-
-    }
-
-    private Alumno getPayload(String id, String uo, String nombre) {
+    private Alumno getPayload(final String id, final String uo, final String nombre) {
         return new Alumno(id, uo, nombre);
     }
 
-    private Alumno getPayloadWithUrl(String id, String uo, String nombre, String url_foto) {
+    private Alumno getPayloadWithUrl(final String id, final String uo, final String nombre, final String url_foto) {
         return new Alumno(id, uo, nombre, url_foto);
     }
 
