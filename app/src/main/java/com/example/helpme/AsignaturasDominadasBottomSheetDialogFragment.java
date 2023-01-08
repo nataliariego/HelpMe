6ad70a4a85e701aa32e526b4141d005e73eba205
FranciscoCoya <kikocoya@gmail.com>
2023-01-08 12:55:48 +0100

    refactor: remove unused code and debug comments on fragment AsignaturasDominadasBottomSheetDialogFragment

--- app/src/main/java/com/example/helpme/AsignaturasDominadasBottomSheetDialogFragment.java
+++ app/src/main/java/com/example/helpme/AsignaturasDominadasBottomSheetDialogFragment.java
@@ -1,11 +1,9 @@
 package com.example.helpme;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.helpme.databinding.FragmentAsignaturasDominadasBottomSheetDialogListDialogBinding;
+import com.example.helpme.model.Asignatura;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.io.Serializable;
@@ -20,27 +19,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import adapter.AsignaturaDominadaSheetAdapter;
 
 /**
- * <p>A fragment that shows a list of items as a modal bottom sheet.</p>
- * <p>You can show this modal bottom sheet from your activity like this:</p>
- * <pre>
- *     AsignaturasDominadasBottomSheetDialogFragment.newInstance(30).show(getSupportFragmentManager(), "dialog");
- * </pre>
+ * Activity (Bottom Sheet) que contiene las asignaturas seleccionadas en
+ * el campo "Selecciona las asignaturas" del activity de Crear una Cuenta.
+ * Al pulsar sobre el botón situado a la derecha del campo anterior, se añade la asignatura
+ * a la lista de asignaturas dominadas y, pulsando el botón "Ver asignaturas seleccionadas",
+ * se muestra esta activity.
  */
 public class AsignaturasDominadasBottomSheetDialogFragment extends BottomSheetDialogFragment {
 
     public static final String TAG = "ASIG_DOM_SHEET_FRAGMENT";
-
-    private RecyclerView recyclerAsignaturasDominadas;
+    private static final String ASIGNATURAS = "asignaturas_dominadas";
 
     private AsignaturaDominadaSheetAdapter asignaturaDominadaSheetAdapter = new AsignaturaDominadaSheetAdapter(new ArrayList<>());
-
     Map<String, Object> asigs = new HashMap<>();
 
-    private static final String ASIGNATURAS = "asignaturas_dominadas";
     private FragmentAsignaturasDominadasBottomSheetDialogListDialogBinding binding;
 
     public static AsignaturasDominadasBottomSheetDialogFragment newInstance(Map<String, Object> asignaturas) {
@@ -53,9 +50,8 @@ public class AsignaturasDominadasBottomSheetDialogFragment extends BottomSheetDi
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
         binding = FragmentAsignaturasDominadasBottomSheetDialogListDialogBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -83,13 +79,14 @@ public class AsignaturasDominadasBottomSheetDialogFragment extends BottomSheetDi
 
         List<String> nombresAsignaturas = new ArrayList<>();
 
-        recyclerAsignaturasDominadas = (RecyclerView) view.findViewById(R.id.recycler_asignaturas_dominadas_sheet_createAccount);
+        RecyclerView recyclerAsignaturasDominadas = (RecyclerView) view.findViewById(R.id.recycler_asignaturas_dominadas_sheet_createAccount);
 
+        assert args != null;
         asigs = (Map<String, Object>) args.getSerializable(ASIGNATURAS);
 
         for (Map.Entry<String, Object> asignatura : asigs.entrySet()) {
             Map<String, Object> asig = (Map<String, Object>) asignatura.getValue();
-            String nombre = asig.get("nombre").toString();
+            String nombre = Objects.requireNonNull(asig.get(Asignatura.NOMBRE)).toString();
             nombresAsignaturas.add(nombre);
         }
 
@@ -99,7 +96,6 @@ public class AsignaturasDominadasBottomSheetDialogFragment extends BottomSheetDi
 
         asignaturaDominadaSheetAdapter = new AsignaturaDominadaSheetAdapter(nombresAsignaturas);
         recyclerAsignaturasDominadas.setAdapter(asignaturaDominadaSheetAdapter);
-
     }
 
     @Override
