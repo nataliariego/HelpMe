f8d28cf4f347b5f5f4f6e5aa2d943c561f1b1a67
FranciscoCoya <kikocoya@gmail.com>
2023-01-08 12:46:54 +0100

    refactor: remove ajustes cuenta activity comments and unused code, remove warnings

--- app/src/main/java/com/example/helpme/AjustesCuentaActivity.java
+++ app/src/main/java/com/example/helpme/AjustesCuentaActivity.java
@@ -1,7 +1,5 @@
 package com.example.helpme;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Objects;
+
 import auth.Authentication;
 import chat.AlumnoStatus;
 import chat.ChatService;
@@ -24,64 +24,62 @@ public class AjustesCuentaActivity extends AppCompatActivity {
     private Button btResetPasswordLink;
     private Button btVerifyAccount;
 
-    private LinearLayout layoutVerificacionEmailLabel;
-
-    private FirebaseUser userInSession = FirebaseAuth.getInstance().getCurrentUser();
+    private final FirebaseUser userInSession = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajustes_cuenta);
         setTitle(R.string.account_settings);
-
         initFields();
     }
 
+    /**
+     * Inicializar los componentes del layout. Si el usuario ya tiene el email verificado
+     * no se le mostrará el botón de verificación de cuenta.
+     *
+     * @see #addListeners()
+     */
     private void initFields() {
         btLogout = (Button) findViewById(R.id.button_logout);
         btResetPasswordLink = (Button) findViewById(R.id.button_reset_password);
         btVerifyAccount = (Button) findViewById(R.id.button_verificar_email);
-        layoutVerificacionEmailLabel = (LinearLayout) findViewById(R.id.layout_correo_verificado_ajustes);
+        LinearLayout layoutVerificacionEmailLabel = (LinearLayout) findViewById(R.id.layout_correo_verificado_ajustes);
 
+        addListeners();
+
+        /* Verificar cuenta */
+        if (Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).isEmailVerified()) {
+            layoutVerificacionEmailLabel.setVisibility(View.VISIBLE);
+            btVerifyAccount.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Añade los listeners necesarios para los elementos del activity.
+     */
+    private void addListeners() {
         /* Cerrar sesión */
-        btLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /* Cambiar estado a OFFLINE */
-                ChatService.getInstance().changeCurrentUserStatus(AlumnoStatus.OFFLINE.toString().toLowerCase(), new ChatService.AlumnoStatusCallback() {
-                    @Override
-                    public void callback() {
-                        Authentication.getInstance().signOut();
-                        redirectToLogin();
-                        finish();
-                    }
-                });
-            }
+        btLogout.setOnClickListener(view -> {
+            /* Cambiar estado a OFFLINE */
+            ChatService.getInstance().changeCurrentUserStatus(AlumnoStatus.OFFLINE.toString().toLowerCase(), () -> {
+                Authentication.getInstance().signOut();
+                redirectToLogin();
+                finish();
+            });
         });
 
         /* Restablecer contraseña cuenta mediante link al correo*/
-        btResetPasswordLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Authentication.getInstance().sendResetPasswordEmail(userInSession.getEmail());
+        btResetPasswordLink.setOnClickListener(view -> {
+            Authentication.getInstance().sendResetPasswordEmail(userInSession.getEmail());
 
-                Toast.makeText(AjustesCuentaActivity.this, R.string.olvida_contraseña, Toast.LENGTH_LONG).show();
-            }
+            Toast.makeText(AjustesCuentaActivity.this, R.string.olvida_contraseña, Toast.LENGTH_LONG).show();
         });
 
-        /* Verificar cuenta */
-        if (FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()) {
-            layoutVerificacionEmailLabel.setVisibility(View.VISIBLE);
-            btVerifyAccount.setVisibility(View.INVISIBLE);
-        }
-
-        btVerifyAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Authentication.getInstance().sendEmailVerification();
-
-                Toast.makeText(AjustesCuentaActivity.this, R.string.revisa_contraseña, Toast.LENGTH_LONG).show();
-            }
+        /* Verificar el correo electrónico de la cuenta */
+        btVerifyAccount.setOnClickListener(view -> {
+            Authentication.getInstance().sendEmailVerification();
+            Toast.makeText(AjustesCuentaActivity.this, R.string.revisa_contraseña, Toast.LENGTH_LONG).show();
         });
     }
 
